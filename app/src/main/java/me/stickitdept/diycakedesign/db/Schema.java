0a1491e5faa5ad4266b97a1c74a45909ea67c416
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-04-15 23:27:41 +0800

    Add initial Schema class

---
+++ app/src/main/java/me/stickitdept/diycakedesign/db/Schema.java
@@ -0,0 +1,8 @@
+package me.stickitdept.diycakedesign.db;
+
+/**
+ * Write all schemas here
+ */
+public class Schema {
+    // public static final String DROP_TBL_ACCOUNTS = "DROP TABLE IF EXISTS " + Table.TBL_ACCOUNTS;
+}
