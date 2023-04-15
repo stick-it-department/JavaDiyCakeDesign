559038babb5b2f2466ee018ea9f2a8947a901110
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-04-15 23:29:32 +0800

    Update initial Instance class

--- app/src/main/java/me/stickitdept/diycakedesign/db/Instance.java
+++ app/src/main/java/me/stickitdept/diycakedesign/db/Instance.java
@@ -1,4 +1,27 @@
 package me.stickitdept.diycakedesign.db;
 
-public class Instance {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Instance extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "Cakes.db";
+    public static final int SCHEMA_VERSION = 1;
+
+    public Instance(@Nullable Context context) {
+        super(context, DB_NAME, null, SCHEMA_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Execute sql queries for creating tables
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Execute sql queries for dropping tables
+    }
 }
