cc106f945559111250162ee98d6e29e0621f5e0a
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-04-15 23:27:16 +0800

    Add initial Table class

---
+++ app/src/main/java/me/stickitdept/diycakedesign/db/Table.java
@@ -0,0 +1,18 @@
+package me.stickitdept.diycakedesign.db;
+
+/**
+ * Write all table names here.
+ *
+ * Rules:
+ *
+ * 1. Variable name should be prefixed with TBL_
+ * 2. Variable name should be all upper case words are separated by underscore.
+ * 3. Variable should be declared as `public static final String`
+ * 4. Name of table should plural
+ * 5. Name of table should be title case
+ *
+ * Example: public static final String TBL_USERS
+ */
+public class Table {
+    // public static final String TBL_USERS = "Users"; -> Example
+}
